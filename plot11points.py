8ac73d73dbb1fb96714aebe8341fffc032f2f389
Eliseo Garcia <jorgeeliseo10@icloud.com>
2026-03-05 02:56:49 -0600

    Better performace + msgs

--- plot11points.py
+++ plot11points.py
@@ -5,10 +5,12 @@ def main() -> None:
     fig = plt.figure(figsize=(7, 4.5))
     ax = fig.add_subplot()
 
-    ax.set_xlim(0, 10)
+    num_of_inputs = 11
+
+    ax.set_xlim(0, num_of_inputs-1)
     ax.set_ylim(0, 10)
 
-    x_vals = list(range(11))
+    x_vals = list(range(num_of_inputs))
     y_vals = []
 
     current_point = {"index": 0}
@@ -16,22 +18,28 @@ def main() -> None:
     plt.subplots_adjust(bottom=0.25)
 
     axbox = plt.axes([0.2, 0.1, 0.6, 0.075])
-    text_box = TextBox(axbox, "Point 1 (0, y): ")
+    text_box = TextBox(axbox, "Point 1: (0, _ ): ")
 
     def submit(text: str) -> None:
+        if text.strip() == "":
+            return
         try:
+            x = current_point["index"]
             y = float(text)
+            print(f"Point successfully plotted on ({x}, {y})")
         except ValueError:
-            print("Please enter a number.")
+            print("Please enter a valid number")
             text_box.set_val("")
+            fig.canvas.draw_idle()
             return
 
         y_vals.append(y)
         current_point["index"] += 1
 
         text_box.set_val("")
-        
-        if current_point["index"] == 11:
+        fig.canvas.draw_idle()
+
+        if current_point["index"] == num_of_inputs:
             ax.plot(x_vals, y_vals, marker="o")
             plt.draw()
             text_box.set_active(False)
@@ -39,11 +47,10 @@ def main() -> None:
         else:
             next_x = current_point["index"]
             text_box.label.set_text(
-                f"Point {next_x + 1} ({next_x}, y): "
+                f"Point {next_x + 1}: ({next_x}, _ ): "
             )
 
     text_box.on_submit(submit)
-
     plt.show()
 
 
