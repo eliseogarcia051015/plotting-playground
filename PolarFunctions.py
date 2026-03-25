1d27d32508830d7ac40dc7ddcd510083b8c032a5
Eliseo Garcia <jorgeeliseo10@icloud.com>
2026-03-25 15:29:08 -0500

    Initial File Commit

---
+++ PolarFunctions.py
@@ -0,0 +1,8 @@
+#implement me :P
+
+""""
+Similar to "function_transformer.py" but instead of being in cartesian coordinates,
+this program will handle functions in the polar plane (r = f(\theta)). I think
+I want to keep the idea of sliders, but look into how that implementation will look
+like given things like petals
+"""
