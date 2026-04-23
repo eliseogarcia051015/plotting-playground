46e674ea3fb85f923cf6f9379995b3321a4b7703
Eliseo Garcia <jorgeeliseo10@icloud.com>
2026-04-23 00:09:19 -0500

    Description added

--- MonteCarloPi.py
+++ MonteCarloPi.py
@@ -1,3 +1,12 @@
 """
 Add description here
 """
+import matplotlib.pyplot as plt
+
+def main():
+    fig = plt.figure(figsize=(6,6))
+    print("")
+    plt.show()
+
+if __name__ == "__main__":
+    main()
