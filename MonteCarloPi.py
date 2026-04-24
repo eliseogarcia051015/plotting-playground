6c5347495421218f65109ed04c68a1637ef0a5b8
Eliseo Garcia <jorgeeliseo10@icloud.com>
2026-04-24 15:33:54 -0500

    figures and axes created. (1) User input (2) Visualization

--- MonteCarloPi.py
+++ MonteCarloPi.py
@@ -1,10 +1,47 @@
 """
-Add description here
+This program uses random points inside a square to estimate the value of pi (Monte Carlo Method).
+Points that fall inside the inscribed circle are counted, and the ratio of
+inside points to total points is used to approximate pi. 
 """
+
 import matplotlib.pyplot as plt
+from matplotlib.ticker import MaxNLocator
+from matplotlib.widgets import TextBox
 
 def main():
-    fig = plt.figure(figsize=(6,6))
+    ncircles = 0
+
+    fig0 = plt.figure(figsize=(6,3))
+    fig0.patch.set_facecolor("white")
+
+    ax_prompt = plt.axes([0.4, 0.5, 0.25, 0.2])
+    prompt_box = TextBox(ax_prompt, "")
+
+    def submit(text):
+        try:
+            ncircles = int(text)
+            plt.close(fig0)
+        except ValueError:
+            print("Not valid input")
+            return
+    prompt_box.on_submit(submit)
+    plt.show()
+
+    fig, ax = plt.subplots(figsize=(7,7))
+    plt.style.use('dark_background')
+    fig.canvas.manager.set_window_title("")
+    ax.axhline(0, color="black", linestyle="--", linewidth=1)
+    ax.axvline(0, color="black", linestyle="--", linewidth=1)
+    ax.set_ylim(-1.1, 1.1)
+    ax.set_xlim(-1.1, 1.1)
+    ax.grid(True)
+    ax.xaxis.set_major_locator(MaxNLocator(integer=True))
+    ax.yaxis.set_major_locator(MaxNLocator(integer=True))
+
+    circle = plt.Circle((0, 0), 1, color='white', fill=False)
+    ax.add_patch(circle)
+    
+
     print("")
     plt.show()
 
