2666fcb796891a59e7cf2dfab8513832d5993875
Eliseo Garcia <jorgeeliseo10@icloud.com>
2026-04-25 18:35:42 -0500

    Windows set up with user input handling

--- MonteCarloPi.py
+++ MonteCarloPi.py
@@ -11,11 +11,17 @@ from matplotlib.widgets import TextBox
 def main():
     ncircles = 0
 
-    fig0 = plt.figure(figsize=(6,3))
-    fig0.patch.set_facecolor("white")
+    fig0 = plt.figure(figsize=(6, 3))
+    fig0.canvas.manager.set_window_title("User input")
 
-    ax_prompt = plt.axes([0.4, 0.5, 0.25, 0.2])
+    fig0.text(0.025, 0.9, "Choose how many circles you would like to use for the visualization", fontsize=12, color='Black')
+    ax = fig0.add_axes([0, 0, 1, 1])
+    #ax.axvline(0.5, color="black", linestyle="--", linewidth=1)#center the porimpt box. maybe lower it as well
+    ax_prompt = plt.axes([0.35, 0.5, 0.30, 0.2])
     prompt_box = TextBox(ax_prompt, "")
+
+    ax.set_xticks([])
+    ax.set_yticks([])
 
     def submit(text):
         try:
@@ -23,13 +29,14 @@ def main():
             plt.close(fig0)
         except ValueError:
             print("Not valid input")
+            prompt_box.set_val("")
             return
     prompt_box.on_submit(submit)
     plt.show()
 
-    fig, ax = plt.subplots(figsize=(7,7))
     plt.style.use('dark_background')
-    fig.canvas.manager.set_window_title("")
+    fig, ax = plt.subplots(figsize=(7,7))
+    fig.canvas.manager.set_window_title("Monte Carlo Method")
     ax.axhline(0, color="black", linestyle="--", linewidth=1)
     ax.axvline(0, color="black", linestyle="--", linewidth=1)
     ax.set_ylim(-1.1, 1.1)
