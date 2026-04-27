2b224163906da1c6fa3f68923a130bc3289212fa
Eliseo Garcia <jorgeeliseo10@icloud.com>
2026-04-27 18:59:50 -0500

    inital commit

---
+++ BasicGravity.py
@@ -0,0 +1,9 @@
+# Try to simulate a ball being dropping 
+import matplotlib.pyplot as plt
+
+def main():
+    fig = plt.figure(figsize=(4,5))
+    plt.show()
+
+if __name__ == "__main__":
+    main()
