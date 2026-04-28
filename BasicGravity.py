f57a93d65072022f364fe8be302920e957347c74
Eliseo Garcia <jorgeeliseo10@icloud.com>
2026-04-28 14:54:09 -0500

    Kinematic equations added

--- BasicGravity.py
+++ BasicGravity.py
@@ -2,31 +2,40 @@
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
+
 def main():
     plt.style.use('dark_background')
     fig = plt.figure(figsize=(4,5))
     fig.canvas.manager.set_window_title("Gravity Simulator")
     ax = fig.add_subplot(111)
-    ax.set_xlim(0,1), ax.set_xticks([])
-    ax.set_ylim(0,1.2), ax.set_yticks([])
-    plt.pause(1)
-
-    x,y = 0.5, 1.1
-    SPEEDY = 0.01
+    ax.set_xlim(0,2), ax.set_xticks([])
+    ax.set_ylim(0,10), ax.set_yticks([])
     
-    ball = ax.scatter([x], [y], color = "white", s=100)
+
+
+    x = 1        #m
+    y0 = 8        #m
+    v0 = 0        #m/s
+    g = -9.81    #m/s^2
+    dt = 0.02    #second/frame
+    
+    ball = ax.scatter([x], [y0], color = "white", s=100)
+    plt.pause(0.75)
 
     def update(frame):
-        nonlocal y, SPEEDY
-        y = y - SPEEDY
+        t = frame * dt
+
+        y = y0 + v0*t + 0.5*g*t**2
+        v = v0 + g*t
         
-        ball.set_offsets([0.5, y])
-        if (y<0):
+        if (y<=0):
             y = 0
-            SPEEDY = 0
+            print(f"Hit ground at t={t:.2f}s with v={v:.2f} m/s")
+            anim.event_source.stop()
+        ball.set_offsets([x, y])
         return ball,
 
-    anim = animation.FuncAnimation(fig, update, frames=100, interval=1, blit=True)
+    anim = animation.FuncAnimation(fig, update, interval=5, blit=True)
     plt.show()
 
 if __name__ == "__main__":
