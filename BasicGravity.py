e8d605bc0679261a37d5475237daad8cec83142c
Eliseo Garcia <jorgeeliseo10@icloud.com>
2026-04-27 19:11:56 -0500

    Ball added

--- BasicGravity.py
+++ BasicGravity.py
@@ -2,7 +2,17 @@
 import matplotlib.pyplot as plt
 
 def main():
+    plt.style.use('dark_background')
     fig = plt.figure(figsize=(4,5))
+    fig.canvas.manager.set_window_title("Gravity Simulator")
+    ax = fig.add_subplot(111)
+    ax.set_xlim(0,1), ax.set_xticks([])
+    ax.set_ylim(0,1.2), ax.set_yticks([])
+    plt.pause(1)
+
+    ax.scatter(0.5, 1, color="white", linewidth=10)
+    fig.canvas.draw()
+
     plt.show()
 
 if __name__ == "__main__":
