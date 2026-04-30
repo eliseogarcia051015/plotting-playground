82b61705c738380598d5b66c1bc47b119fc17ab6
Eliseo Garcia <jorgeeliseo10@icloud.com>
2026-04-30 14:40:04 -0500

    offset added to ball to make reference at the bottom of the ball instead of the center

--- BasicGravity.py
+++ BasicGravity.py
@@ -8,23 +8,25 @@ def main():
     fig = plt.figure(figsize=(4,5))
     fig.canvas.manager.set_window_title("Gravity Simulator")
     ax = fig.add_subplot(111)
+    window_y_limit = 10
     ax.set_xlim(0,2), ax.set_xticks([])
-    ax.set_ylim(0,10), ax.set_yticks([])
+    ax.set_ylim(0,window_y_limit), ax.set_yticks([])
     
     x = 1        #m
-    y = 8       #m
-    v = 0.00       #m/s
+    y = 8        #m
+    v = 0.00     #m/s
     g = -9.81    #m/s^2
-    e = .73    #about the COR for a tennis ball
-    t = 0.00
+    e = .73      #about the COR for a tennis ball
+    t = 0.00     #s
     dt = 0.02    #second/frame
 
     time_txt = ax.text(0, 10.7, f'Time: {t}')
     vel_txt = ax.text(0.7, 10.7, f'Velocity: {v}')
     height_txt = ax.text(1.4, 10.7, f"Height: {y}")
     
-    ball = ax.scatter([x], [y], color = "white", s=100)
-    plt.pause(0.75)
+    r=0.3 #ball radius
+    ball = ax.scatter([x], [y+r], color = "white", edgecolors='blue', s=100)
+    plt.pause(1.5)
 
     def update(frame):
         nonlocal y, v, t
@@ -45,10 +47,10 @@ def main():
                 v = 0
                 anim.event_source.stop()
         time_txt.set_text(f'Time: {t:.2f}')
-        height_txt.set_text(f'Height: {y:.2f}')
+        height_txt.set_text(f'Height: {y:.0f}')
         vel_txt.set_text(f'Velocity: {v:.2f}')
         fig.canvas.draw()
-        ball.set_offsets([x, y])
+        ball.set_offsets([x, y+r])
         return ball, time_txt
 
     anim = animation.FuncAnimation(fig, update, interval=5, blit=True)
