c764df5a61bcbe63ab5a767edf54a55a3e0878fe
Eliseo Garcia <jorgeeliseo10@icloud.com>
2026-05-02 08:47:12 -0500

    HilbertCurve.py

---
+++ HilbertCurve.py
@@ -0,0 +1,7 @@
+"""
+This program visualizes the Hilbert curve, a type of space-filling curve
+that maps a one-dimensional line into a two-dimensional area. The curve is
+generated recursively, with each iteration increasing its complexity and
+level of detail. Despite being a continuous line, the Hilbert curve passes
+through every point in a grid without crossing itself.
+"""
