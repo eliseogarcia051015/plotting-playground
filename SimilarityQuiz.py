bfe8883bc7439dcd545653610e39366412831006
Eliseo Garcia <jorgeeliseo10@icloud.com>
2026-05-14 11:08:09 -0500

    added basic questions with answeres stored in an array

---
+++ SimilarityQuiz.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+def similarity(v1, v2):
+    return np.dot(v1, v2) / np.linalg.norm(v1,v2)
+
+def main():
+    print("This is a simple test")
+    responses = []
+    users = 2
+    for i in range(users):
+        print(f"\nUser {i+1} Quiz")
+        question1 = int(input("Do you like pizza (1-5): "))
+        question2 = int(input("Do you like dogs (1-5): "))
+        question3 = int(input("Do you like the outdoors (1-5): "))
+        question4 = int(input("Do you like sports (1-5): "))
+        response = np.array([question1, question2, question3, question4])
+        responses.append(response)
+    print(responses)
+
+
+if __name__ == "__main__":
+    main()
