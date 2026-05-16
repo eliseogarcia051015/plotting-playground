a736c955f0830ae8ec32313a67ff4ce6f80b5bff
Eliseo Garcia <jorgeeliseo10@icloud.com>
2026-05-15 22:16:03 -0500

    Questios are now in a dictionary / for loop for questions

--- SimilarityQuiz.py
+++ SimilarityQuiz.py
@@ -8,16 +8,21 @@ def similarity(v1, v2):
 
 def main():
     print("This is a simple test")
+    questions = {   
+        "0": "Do you like pizza (1-5): ",
+        "1": "Do you like dogs (1-5): ",
+        "2": "Do you like the outdoors (1-5): ",
+        "3": "Do you like sports (1-5): ",
+    }
     responses = []
     users = 2
     for i in range(users):
         print(f"\nUser {i+1} Quiz")
-        question1 = int(input("Do you like pizza (1-5): "))
-        question2 = int(input("Do you like dogs (1-5): "))
-        question3 = int(input("Do you like the outdoors (1-5): "))
-        question4 = int(input("Do you like sports (1-5): "))
-        response = np.array([question1, question2, question3, question4])
-        responses.append(response)
+        user_answer = []
+        for q in questions:
+            answer = int(input(questions[f"{q}"]))
+            user_answer.append(answer)
+        responses.append(np.array(user_answer))
     print(responses)
     print(f"\nSimilarity: {similarity(responses[0],responses[1])*100:.2f}%")
 
