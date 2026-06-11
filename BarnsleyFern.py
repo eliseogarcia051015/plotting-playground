ca390867f06f462249fec94ea7dcb71debb81290
Eliseo Garcia <jorgeeliseo10@icloud.com>
2026-06-10 22:35:06 -0500

    Inital commit

--- BarnsleyFern.py
+++ BarnsleyFern.py
@@ -1,1 +1,5 @@
-#Implement later#
+"""
+This program generates the Barnsley fern, a classic example of a fractal
+that mimics the appearance of a natural fern leaf. It is created using an
+Iterated Function System (IFS)
+"""
