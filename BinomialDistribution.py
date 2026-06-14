4df432eeef4f86968f4bf76601da68334df02af1
Eliseo Garcia <jorgeeliseo10@icloud.com>
2026-06-13 23:37:40 -0500

    description added

---
+++ BinomialDistribution.py
@@ -0,0 +1,7 @@
+"""
+This program visualizes a simplified Galton board. Ballsfall through pins making a random 
+left or right move at eachpeg with equal probability. After passing 
+through all rows, each ball lands in a bin and stacks on top of previously landed balls. 
+As more balls are dropped, the distribution of balls across the bins approaches a binomial
+distribution, forming a bell-shaped curve.
+"""
