0c0cfa4cfb821f4e80efce2db61c27be63b7f363
Eliseo Garcia <jorgeeliseo10@icloud.com>
2026-06-14 01:52:47 -0500

    window pop up

--- BinomialDistribution.py
+++ BinomialDistribution.py
@@ -5,3 +5,16 @@ through all rows, each ball lands in a bin and stacks on top of previously lande
 As more balls are dropped, the distribution of balls across the bins approaches a binomial
 distribution, forming a bell-shaped curve.
 """
+
+import matplotlib.pyplot as plt
+
+def main():
+    fig, ax = plt.subplots(figsize=(6,6))
+    fig.canvas.manager.set_window_title("Galton board")
+    plt.xticks([])
+    plt.yticks([])
+
+    plt.show()
+
+if __name__ == "__main__":
+    main()
